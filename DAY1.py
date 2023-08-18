a4a67c19c12b1b8494ea87ffb65653bc1dca6591
alittlebitsleepy <135020326+alittlebitsleepy@users.noreply.github.com>
2023-08-18 10:36:30 +0800

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import json,time,requests
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("txt")
+        body = json.dumps(
+            {"version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+             "input": {
+                 "prompt": q
+             }
+             }
+        )
+        headers = {
+            'Authorization': 'Token r8_2hNkZBRzrCn4HqanHfwBMtqhTFDyWb04Ih6AX',
+            'Content-Type': 'application/json'
+        }
+        output = requests.post('https://api.replicate.com/v1/predictions', data=body, headers=headers)
+        time.sleep(10)
+        get_url = output.json()['urls']['get']
+        get_result = requests.post(get_url, headers=headers).json()['output']
+        image = Image.open(requests.get(get_result[0], stream=True).raw)
+        image.show()
+        return render_template("index.html", result=get_result[0])
+    else:
+        return render_template("index.html", result="waiting")
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
